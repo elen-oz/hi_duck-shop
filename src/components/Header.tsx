7ceaac451c1e27adc1ca951c79252a701e3df37f
elen-oz <ozerova.lenka@gmail.com>
2024-01-13 21:28:36 +0100

    feat: add responsiveness to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,15 @@ interface Props {
 
 const Header = ({ cartItems }: Props) => {
   return (
-    <header className="flex justify-between bg-main px-8 py-4 text-white">
-      <Link to="/">
-        <h2 className="text-3xl">
-          <span className="text-accent">Duck</span> Shop
-        </h2>
-      </Link>
-      <Cart cartItems={cartItems} />
+    <header className="w-full bg-main ">
+      <div className="mx-auto flex justify-between px-8 py-4 text-white md:container">
+        <Link to="/">
+          <h2 className="text-3xl">
+            <span className="text-accent">Duck</span> Shop
+          </h2>
+        </Link>
+        <Cart cartItems={cartItems} />
+      </div>
     </header>
   );
 };
