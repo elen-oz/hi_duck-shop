e652d715b797f2e0882f652953ca26232684dbef
elen-oz <ozerova.lenka@gmail.com>
2024-01-14 21:59:01 +0100

    refactor: temp remove cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,18 +1,18 @@
-import { useContext } from "react";
-import CartContext from "../context/cartContext";
-import { Product } from "../App";
+// import { useContext } from "react";
+// import CartContext from "../context/cartContext";
+// import { Product } from "../App";
 
-const CartPage = () => {
-  const cartCtx = useContext(CartContext);
+// const CartPage = () => {
+//   const cartCtx = useContext(CartContext);
 
-  const cartItems = cartCtx?.items || [];
-  const totalPrice = cartCtx?.totalAmount || 0;
+//   const cartItems = cartCtx?.items || [];
+//   const totalPrice = cartCtx?.totalAmount || 0;
 
-  const totalQuantity = cartItems.reduce(
-    (acc, item) => acc + (item.amount || 0),
-    0,
-  );
+//   const totalQuantity = cartItems.reduce(
+//     (acc, item) => acc + (item.amount || 0),
+//     0,
+//   );
 
-  return <div>CartPage</div>;
-};
-export default CartPage;
+//   return <div>CartPage</div>;
+// };
+// export default CartPage;
