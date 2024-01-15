cb58a710629ab1549213c1feba8f0b6c911a0c85
elen-oz <ozerova.lenka@gmail.com>
2024-01-15 18:19:11 +0100

    fix building

--- src/pages/MainWrapperPage.tsx
+++ src/pages/MainWrapperPage.tsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useContext } from "react";
-import CartContext from "../context/cartContext";
 
 const MainWrapperPage = () => {
   return (
